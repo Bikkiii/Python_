2eb14c9b769bfbb7d8a38aac0bd6cc171233555b
Bikkiii <bikashdhami15111@gmail.com>
2025-07-12 18:57:33 +0545

    Uses Mathematical operators

---
+++ day2/Mathematical Operations.py
@@ -0,0 +1,16 @@
+# 1+2
+# 3-2
+# 3*2
+# 2**3(2^3)
+print(type(6/2))
+
+# PEMDAS LR
+# Paranthesis ()
+# Exponent **
+# Multiplication *
+# Division /
+# Additon +
+# Subtraction -
+
+print(3 *  3 + 3 / 3 - 3)
+print(3 *  (3 + 3) / 3 - 3)

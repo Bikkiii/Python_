94382d9386b0ab1424a093b1e2de013ed4a7447f
Bikkiii <bikashdhami15111@gmail.com>
2025-07-12 17:44:28 +0545

    Learned about primtive Data Types

---
+++ day2/Data_Types.py
@@ -0,0 +1,28 @@
+#Data types
+
+
+#string
+
+print("Hello"[0])
+print("Hello"[4])
+print("Hello"[1])
+
+print("123"+" 345")
+print("Hello"+" world")
+
+#integers
+
+print(123+123)
+a=2
+b=4
+print(a+b)
+
+#floats
+
+3.14159
+print(a+3.14159)
+
+#boolean 
+
+True
+False

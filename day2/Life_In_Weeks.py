47dd463fc330d2fc17563b594901e2f842902fd4
Bikkiii <bikashdhami15111@gmail.com>
2025-07-12 19:31:53 +0545

    Nnumber Manipulation exercise

---
+++ day2/Life_In_Weeks.py
@@ -0,0 +1,7 @@
+age=int(input(("What is your current age: ")))
+rem_age=84-age
+days=rem_age * 365
+weeks=rem_age * 52
+months= rem_age *12
+
+print(f"You have {days} days, {weeks} weeks, {months} months left to reach 84")

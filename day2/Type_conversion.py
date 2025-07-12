91005f7162c253cc0a41986cd94201c0f4d57022
Bikkiii <bikashdhami15111@gmail.com>
2025-07-12 18:29:25 +0545

    Data type conversion

---
+++ day2/Type_conversion.py
@@ -0,0 +1,14 @@
+num_char=len(input("what is your name?\n"))
+# print("Your name has "+ num_char +" chara
+
+#type casting
+a=123
+print(type(a))
+
+a=str(123)
+print(type(a))
+new_num_char=str(num_char)
+print("Your name has " + new_num_char + " Characters")
+
+print(20+ float("2.55"))
+print(str(10)+str(20))

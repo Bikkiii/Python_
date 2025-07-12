11ec94a8e4c9f98f7dd9249e28319ac63bc0eb1e
Bikkiii <bikashdhami15111@gmail.com>
2025-07-12 18:47:54 +0545

    Data Type Conversion

--- day2/Type_conversion.py
+++ day2/Type_conversion.py
@@ -1,14 +1,25 @@
-num_char=len(input("what is your name?\n"))
-# print("Your name has "+ num_char +" chara
+# num_char=len(input("what is your name?\n"))
+# # print("Your name has "+ num_char +" chara
 
-#type casting
-a=123
-print(type(a))
+# #type casting
+# a=123
+# print(type(a))
 
-a=str(123)
-print(type(a))
-new_num_char=str(num_char)
-print("Your name has " + new_num_char + " Characters")
+# a=str(123)
+# print(type(a))
+# new_num_char=str(num_char)
+# print("Your name has " + new_num_char + " Characters")
 
-print(20+ float("2.55"))
-print(str(10)+str(20))
+# print(20+ float("2.55"))
+# print(str(10)+str(20))
+
+#exercise
+
+two_digit_number=input("Enter any two digit number: ")
+
+first_digit= two_digit_number[0]    #accesing first index element
+second_digit=two_digit_number[1]    #accerssing second index element
+
+result=int(first_digit) + int(second_digit)     #adding 
+print(result)
+

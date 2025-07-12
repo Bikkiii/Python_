a8609f8e4e0c91a7da2ba07001aa09734c8151cb
Bikkiii <bikashdhami15111@gmail.com>
2025-07-12 19:20:13 +0545

    Number Manipulation and f-strings

---
+++ day2/Number_Manipualtion.py
@@ -0,0 +1,18 @@
+# print(round(8/3))     Round off
+# print(8//3)           Round Off
+# print(round(8/3,2))   Rounding off to 2 decimal places
+
+result=4/2
+result/=2
+# result+=2
+# result-=2
+# result*=2
+print(result)
+
+score=0
+height=1.8
+isWinning=True
+print("Yor score is " + str(score))
+
+#f-strings
+print(f"Your score is {score}, your height is {height}, you are winning is {isWinning}")

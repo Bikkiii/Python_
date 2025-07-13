2d7819538b1b7b1689a574819d82e5b7f8e993d5
Bikkiii <bikashdhami15111@gmail.com>
2025-07-13 17:33:09 +0545

    Fun island game

--- day3/treasure_island.py
+++ day3/treasure_island.py
@@ -1,10 +1,30 @@
+print('''*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/[TomekK]
+*******************************************************************************''')
 print("Welcome to Treasure Island\nYour mission is to find the treasure")
-move=input("Which direction do you want to go? L or R: ")
-if move == "L":
-   swim=input("You are in the sea, do you want to swim or wait? S or W: ")
-   if swim=="W":
-       door=input("You are last step away to find the treasure\nThere are three doors(RED,BLUE,YELLOW)\nWhich door do you want to open? R,B or Y: ")
-       if door=="Y":
+move=input("Which direction do you want to go? L or R: ").lower()
+if move == "l":
+   swim=input("You are in the sea, do you want to swim or wait? S or W: ").lower()
+   if swim=="w":
+       door=input("You are last step away to find the treasure\nThere are three doors(RED,BLUE,YELLOW)\nWhich door do you want to open? R,B or Y: ").lower()
+       if door=="y":
            print("Congrats!!, You won the game")
        else:
            print("Game Over!!!")

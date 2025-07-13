8747c0516903ebf9e4b5b8c3a5f462cfcd5212f4
Bikkiii <bikashdhami15111@gmail.com>
2025-07-13 15:20:48 +0545

    If else condition

---
+++ day3/conditional_operators.py
@@ -0,0 +1,16 @@
+# >  Greater than
+# <  Less than
+# >=  Greater than or equal to
+# <=  Less than or equal To
+# ==  Equal to
+# !=  Not equal to
+
+
+print("Welcome to the rollercoaster!")
+height=int(input("What is oyur height in cm? "))
+
+if height >= 120:
+    print("You can ride rollercoaster.")
+else:
+    print("You can't ride rollercoaster.")
+        

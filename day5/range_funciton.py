c95005e7e923e078c60c85d6a404904d3d9f777d
Bikkiii <bikashdhami15111@gmail.com>
2025-07-15 22:48:09 +0545

    Learn range function

---
+++ day5/range_funciton.py
@@ -0,0 +1,13 @@
+# for i in range(a,b):
+#     print(number)
+
+for number in range(1, 10): # drom 1 to 9(exclude 10)
+    print(number)
+
+for number in range(1,11,3):  #steps=3
+    print(number)    
+    
+total=0    
+for i in range(1,101):
+    total += i
+print(total)        

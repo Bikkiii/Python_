137e9abd969ace6a1e30ab9eee02c206a7787e6d
Bikkiii <bikashdhami15111@gmail.com>
2025-07-15 17:54:16 +0545

    Learn Nested Lists

---
+++ day4/Nested_Lists.py
@@ -0,0 +1,11 @@
+# Districts_of_Nepal= ["Kanchanpur","Kathmandu","Lalitpur","Kailali","Nepalgunj","Chitwan"]
+
+# num_of_districts= len(Districts_of_Nepal
+# print(num_of_districts)
+
+
+fruits=["Strawberry,","Nectarines,","Apples,","Grapes,","Peaches,","Cherries,","Pears"]
+vegetables=["Spinach,","Kale,","Tomatoes,","Celery,","Potatoes,"]
+
+dirty_dozen= [fruits,vegetables]
+print(dirty_dozen)

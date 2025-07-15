65fde9041049011ab6616af381898f375c031088
Bikkiii <bikashdhami15111@gmail.com>
2025-07-15 17:05:17 +0545

    Learn about random function

---
+++ day4/Random.py
@@ -0,0 +1,10 @@
+import random
+
+random_integer= random.randint(1,10)   #generate random number from 1 to 10 including 1 and 10(Throws error if a>b)
+print(random_integer)
+
+random_float= random.random()           #generate random number between 0and 1 excluding 0 and 1
+print(random_float)
+
+# love_score= random.randint(1,100)
+# print(f"Your love score is {love_score}")

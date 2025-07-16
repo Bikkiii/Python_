4ef2c46f7f688c032ff47f5c36e0eef6edee8240
Bikkiii <bikashdhami15111@gmail.com>
2025-07-16 16:23:00 +0545

    Learn Function Implementation

---
+++ day6/3.py
@@ -0,0 +1,29 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# def turn_around():
+#     turn_left()
+#     turn_left()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()    
+#     turn_left()
+
+# def loop():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# loop()
+# loop()
+# loop()
+# loop()
+# loop()
+# loop()
+
+# for i in range(6):
+#     loop()

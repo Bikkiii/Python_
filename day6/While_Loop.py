69825e0862a15618e4ae6524f9eba8dd641f22b7
Bikkiii <bikashdhami15111@gmail.com>
2025-07-16 16:44:06 +0545

    Learn while looping

---
+++ day6/While_Loop.py
@@ -0,0 +1,31 @@
+# def turn_around():
+#     turn_left()
+#     turn_left()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()    
+#     turn_left()
+
+# def loop():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# num_loop=6
+# while num_loop>0 :
+#     loop()
+#     num_loop-=1
+    
+# while 5> 2:         #infinite loop
+#     print("Hello")
+    
+a=10
+while a>=0:
+    print(a)
+    a-=1

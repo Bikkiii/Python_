11f0312a3768b0afa978d4388c4cc99b8d37bf4e
Bikkiii <bikashdhami15111@gmail.com>
2025-07-16 17:52:58 +0545

    Complete Hurdle challenge 4

---
+++ day6/Hurdle_challenge-4.py
@@ -0,0 +1,34 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+# def turn_around():
+#     turn_left()
+#     turn_left()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()    
+#     turn_left()
+
+# def loop():
+#     turn_left()
+#     while wall_on_right()==True:
+#         move()
+        
+#     turn_right()
+#     move()
+#     turn_right()
+#     while wall_in_front()==False:
+#         move()
+#     if wall_in_front()==True:
+#         turn_left()
+#         if wall_in_front()==True:
+#             loop()
+        
+         
+# while not at_goal():
+#     if wall_in_front()==True:
+#         loop()
+#     else:
+#         move()
+            
+    

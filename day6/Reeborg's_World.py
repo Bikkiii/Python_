2099dc078c8d028d936939e64e44b5c33bfdbb52
Bikkiii <bikashdhami15111@gmail.com>
2025-07-16 16:10:35 +0545

    Learn Funciton Implementation

---
+++ day6/Reeborg's_World.py
@@ -0,0 +1,21 @@
+#open this link
+https://reeborg.ca/reeborg.html
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()    
+    turn_left()
+    
+    
+turn_left()    
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()

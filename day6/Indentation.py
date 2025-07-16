6cf62019fcb2ec4c843bd9d9551edcd205fbbd78
Bikkiii <bikashdhami15111@gmail.com>
2025-07-16 16:32:35 +0545

    Spaces Over Tabs

---
+++ day6/Indentation.py
@@ -0,0 +1,14 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# def my_function():    #Throws an erorr
+#     print("Hello")
+#      print("Bye")     
+
+for i in range():
+    print(i)
+
+# for i in range(2):    #Throws an error
+# print(i)    
+

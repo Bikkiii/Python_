747d81bb9aab870f790529f22226119d839ff67e
Bikkiii <bikashdhami15111@gmail.com>
2025-07-17 18:25:08 +0545

    Create a program to match a guess letter in a word

--- day7/Guess_a_letter.py
+++ day7/Guess_a_letter.py
@@ -4,12 +4,17 @@ word=random.choice(word_list)
 user_input=input("Guess the word(Enter a letter): ").lower()
 
 
-j=0
-while j <= len(word)-1:
-        if(user_input==word[j]):
-            print("Right")
-        else:
-            print("Wrong")    
-        j+=1    
+for i in word:
+    if i==user_input:
+        print("Right")
+    else:
+        print("Wrong")    
+# j=0
+# while j <= len(word)-1:
+#         if(user_input==word[j]):
+#             print("Right")
+#         else:
+#             print("Wrong")    
+#         j+=1    
               
 print(word)

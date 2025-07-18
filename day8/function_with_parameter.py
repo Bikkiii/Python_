f87ab0f7a1df5cc4f67a3b6be7b0f3e4189773e6
Bikkiii <bikashdhami15111@gmail.com>
2025-07-18 20:16:03 +0545

    Learn Function with parameter

---
+++ day8/function_with_parameter.py
@@ -0,0 +1,18 @@
+# def great(something):
+#     print("Hello!, something")
+#     print("what's up!, something")
+#     print("How do you do, something")
+    
+# great()
+
+def names(name):        #parameter
+    print(f"Hello!, {name}")
+    print(f"what's up!, {name}")
+    print(f"How do you do, {name}")
+names("Mama")           #passing_argument
+names("Nobita")
+names("Niggs")
+
+
+#In the above code "Mama","Nobita","Niggs" are arguments(Real values) and "name" is parameter
+

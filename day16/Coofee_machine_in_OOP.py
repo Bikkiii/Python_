5aa612a10260b8c1c5943dedb9239da9f6a71cd6
Bikkiii <bikashdhami15111@gmail.com>
2025-07-28 18:26:16 +0545

    Coffee Machine in OOP

--- day16/Coofee_machine_in_OOP.py
+++ day16/Coofee_machine_in_OOP.py
@@ -3,4 +3,27 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
-print(MenuItem)
+money_machine= MoneyMachine()   #creating an object from class
+coffee_maker=CoffeeMaker()
+menu=Menu()
+
+is_on=True
+
+coffee_maker.report()
+money_machine.report()
+
+while is_on:
+    options= menu.get_items()
+    choice=input(f"What whould you like? ({options}):")
+    if choice== "off":
+        is_on=False
+    elif choice=="report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink=menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+        
+        

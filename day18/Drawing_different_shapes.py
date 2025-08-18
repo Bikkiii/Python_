db9c8908fa452d78d58213feea9c0b65644e82e5
Bikkiii <bikashdhami15111@gmail.com>
2025-08-18 17:56:25 +0545

    Draw different shapes using turtle

---
+++ day18/Drawing_different_shapes.py
@@ -0,0 +1,17 @@
+from turtle import Turtle,Screen
+
+t1=Turtle()
+t1.shape('turtle')
+
+colors=['yellow','red','black','orange','pink','green','brown','purple']
+def shape(n,c):
+    t1.color(c)
+    deg=360/n
+    for i in range(n):
+        t1.forward(100)
+        t1.right(deg)
+        
+        
+for i in range(3,11):
+    shape(i,colors[i-3])    
+       
